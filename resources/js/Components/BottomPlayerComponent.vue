abdebdef07f786d2f770e4cc58a590c8b68a4a82
Zsofenszki Kristóf <zsofenszki.kristof@gmail.com>
2026-02-05 14:29:19 +0100

    Enhance BottomPlayerComponent with playback controls and volume management

--- resources/js/Components/BottomPlayerComponent.vue
+++ resources/js/Components/BottomPlayerComponent.vue
@@ -1,18 +1,74 @@
 <script setup>
-import { Heart, Headphones, HeadphoneOff } from 'lucide-vue-next';
+import {
+    Heart,
+    Headphones,
+    HeadphoneOff,
+    Shuffle,
+    SkipBack,
+    SkipForward,
+    Play,
+    Pause,
+    Repeat2,
+} from 'lucide-vue-next';
 import { SliderRange, SliderRoot, SliderThumb, SliderTrack } from 'reka-ui';
-import { ref, watch } from 'vue';
+import { computed, ref, watch } from 'vue';
 
-const muted = ref(false);
+const isMuted = ref(false);
+const playing = ref(false);
+const isShuffled = ref(false);
+const isRepeated = ref(false);
+const progress = ref([0]);
+
 const volumeSliderValue = ref([20]);
-watch(volumeSliderValue, (volumeSliderValue) => {
-    console.log(volumeSliderValue);
+const startAndStopPlaying = () => {
+    playing.value = !playing.value;
+};
+const changeRepeatedStatus = () => {
+    isRepeated.value = !isRepeated.value;
+};
+
+const changeRepeatedShuffled = () => {
+    isShuffled.value = !isShuffled.value;
+};
+
+const nextSong = () => {
+    console.log('next song');
+};
+
+const prevSong = () => {
+    console.log('prev song');
+};
+
+const currentTimeLabel = computed(() => {
+    const totalSeconds = Math.floor(13);
+
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 });
+
+const durationLabel = computed(() => {
+    const totalSeconds = Math.floor(130);
+
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+});
+
+watch(volumeSliderValue, (volumeSliderValue) => {
+    isMuted.value = volumeSliderValue[0] === 0;
+});
+
+watch(isMuted, () => {
+    volumeSliderValue.value[0] = 0;
+})
 </script>
 
 <template>
     <div
-        class="mx-1 grid h-20 w-full grid-cols-[auto_1fr_auto] items-center rounded bg-ui-surface px-3 text-ui-text"
+        class="mx-1 grid h-24 w-full grid-cols-[auto_1fr_auto] items-center rounded bg-ui-surface px-3 text-ui-text"
     >
         <div class="flex flex-row items-center justify-start gap-2">
             <img
@@ -27,18 +83,93 @@ watch(volumeSliderValue, (volumeSliderValue) => {
             </div>
             <Heart class="fill-red-800" />
         </div>
-        <div class="justify-self-center">Közép</div>
+        <div class="w-full justify-self-center">
+            <div class="flex h-full flex-col items-center space-y-3">
+                <div class="flex flex-row items-center gap-6">
+                    <div
+                        class="relative cursor-pointer transition-transform duration-300 hover:scale-110 hover:text-ui-muted"
+                        :class="{
+                            'text-ui-accent-primary': isShuffled,
+                        }"
+                        @click="changeRepeatedShuffled"
+                    >
+                        <Shuffle />
+                        <span
+                            v-show="isShuffled"
+                            class="absolute -bottom-2 left-1/2 h-1 w-1 -translate-x-1/2 rounded-full bg-ui-accent-primary"
+                        ></span>
+                    </div>
+                    <div
+                        class="cursor-pointer transition-transform duration-300 hover:scale-110 hover:text-ui-muted"
+                        @click="prevSong"
+                    >
+                        <SkipBack />
+                    </div>
+                    <div
+                        class="cursor-pointer rounded-full bg-ui-accent-primary p-2 transition-transform duration-300 hover:scale-110"
+                        @click="startAndStopPlaying"
+                    >
+                        <Play v-show="!playing" class="fill-current" />
+                        <Pause v-show="playing" class="fill-current" />
+                    </div>
+                    <div
+                        class="cursor-pointer transition-transform duration-300 hover:scale-110 hover:text-ui-muted"
+                        @click="nextSong"
+                    >
+                        <SkipForward />
+                    </div>
+                    <div
+                        class="relative cursor-pointer transition-transform duration-300 hover:scale-110 hover:text-ui-muted"
+                        :class="{
+                            'text-ui-accent-primary': isRepeated,
+                        }"
+                        @click="changeRepeatedStatus"
+                    >
+                        <Repeat2 />
+                        <span
+                            v-show="isRepeated"
+                            class="absolute -bottom-2 left-1/2 h-1 w-1 -translate-x-1/2 rounded-full bg-ui-accent-primary"
+                        ></span>
+                    </div>
+                </div>
+                <div
+                    class="flex w-[50%] flex-row items-center justify-between gap-3"
+                >
+                    {{ currentTimeLabel }}
+                    <SliderRoot
+                        v-model="progress"
+                        class="group relative flex h-2 w-full cursor-pointer touch-none select-none items-center gap-3"
+                        :max="100"
+                        :step="1"
+                    >
+                        <SliderTrack
+                            class="relative h-2 grow rounded-full bg-stone-500/30"
+                        >
+                            <SliderRange
+                                class="absolute h-full rounded-full bg-ui-accent-primary"
+                            />
+                        </SliderTrack>
+
+                        <SliderThumb
+                            class="block h-4 w-4 rounded-full bg-white opacity-0 shadow-sm hover:bg-stone-50 focus:shadow-[0_0_0_2px] focus:shadow-ui-accent-primary focus:outline-none group-hover:opacity-100"
+                            aria-label="Progress"
+                        />
+                    </SliderRoot>
+                    {{ durationLabel }}
+                </div>
+            </div>
+        </div>
         <div class="flex flex-row items-center gap-3">
             <div
-                class="cursor-pointer rounded p-1 duration-200 hover:bg-ui-accent-secondary"
+                class="cursor-pointer p-1 transition-transform duration-200 hover:scale-110"
                 @click="
                     () => {
-                        muted = !muted;
+                        isMuted = !isMuted;
                     }
                 "
             >
-                <Headphones v-show="!muted" />
-                <HeadphoneOff v-show="muted" />
+                <Headphones v-show="!isMuted" />
+                <HeadphoneOff v-show="isMuted" />
             </div>
             <SliderRoot
                 v-model="volumeSliderValue"
