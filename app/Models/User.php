c3bb68fe3d6fee3a9dd4eb62c77a9caf65824d82
Zsofenszki Kristóf <zsofenszki.kristof@gmail.com>
2026-02-17 14:29:15 +0100

    Add `favorites` relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -50,5 +52,10 @@ class User extends Authenticatable
     public function artist(): HasOne
     {
         return $this->hasOne(Artist::class);
+    }
+
+    public function favorites(): BelongsToMany
+    {
+        return $this->belongsToMany(Track::class)->withTimestamps()->as('favorites');
     }
 }
